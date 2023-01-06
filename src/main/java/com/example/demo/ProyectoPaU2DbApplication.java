dbb12f4dd970104bd6f947c918bb050f82a24c2d
DavidBoada08 <dfboadaa@uce.edu.ec>
2023-01-05 23:03:12 -0500

    taller17

---
+++ src/main/java/com/example/demo/ProyectoPaU2DbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaU2DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaU2DbApplication.class, args);
+	}
+
+}
